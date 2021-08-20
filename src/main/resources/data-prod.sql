4661802b2f5baa792d7fbf528d4d55aa45be4106
DerekLeungHoWang <hkz88i00123@gmail.com>
2021-08-20 23:02:53 +0800

    fix

--- src/main/resources/data-prod.sql
+++ src/main/resources/data-prod.sql
@@ -1,6 +1,6 @@
 
 
-INSERT INTO users(email,enabled,password,username,provider) VALUES('hkz88i001231@gmail.com',1,'$2a$10$ideoxDhZo1MP9eW.A4UC0.4iPeDeCNM9yVMGQFpzTz04OZp9nxw5S','Derek','local');
+INSERT INTO users(email,enabled,password,username,provider) VALUES('test@test.com',1,'$2a$10$ideoxDhZo1MP9eW.A4UC0.4iPeDeCNM9yVMGQFpzTz04OZp9nxw5S','Derek','local');
 
 INSERT INTO roles(name) VALUES('ROLE_USER');
 INSERT INTO roles(name) VALUES('ROLE_MODERATOR');
@@ -28,35 +28,35 @@ INSERT INTO products(img,price) VALUES ('/static/media/melon_2.5b86e6f7.jpeg',23
  
 SET foreign_key_checks = 0;
 
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (5,'en','Peach','Japan','Yamanashi Premium Peach',NULL);
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (5,'zh','桃','日本','一宮桃-黑盒',NULL);
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (5,'en','Peach','JP','Japan','Yamanashi Premium Peach',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (5,'zh','桃','JP','日本','一宮桃-黑盒',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
 
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (15,'en','Persimmon','New Zeland','New Zeland persimmon',NULL);
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (15,'zh','柿','紐西蘭','紐西蘭柿 (一箱 15裝)',NULL);
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (15,'en','Persimmon','NZ','New Zeland','New Zeland persimmon',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (15,'zh','柿','NZ','紐西蘭','紐西蘭柿 (一箱 15裝)',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
 
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (25,'en','Mandarin','Japan','Karatsu mandarin',NULL);
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (25,'zh','柑','日本','唐津柑',NULL);
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (25,'en','Mandarin','JP','Japan','Karatsu mandarin',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (25,'zh','柑','JP','日本','唐津柑',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
 
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (35,'en','Grapes','Japan','Kyoho grapes Jelly',NULL);
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (35,'zh','提子','日本','日本巨峰波波提',NULL);
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (35,'en','Grapes','JP','Japan','Kyoho grapes Jelly',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (35,'zh','提子','JP','日本','日本巨峰波波提',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
 
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (45,'en','avocado','Australia','Australia avocado',NULL);
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (45,'zh','牛油果','澳洲','澳洲牛油果4個',NULL);
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (45,'en','Avocado','AU','Australia','Australia avocado',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (45,'zh','牛油果','AU','澳洲','澳洲牛油果4個',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
 
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (55,'en','Peach','United States','America Donut Peach',NULL);
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (55,'zh','桃','美國','美國扁桃',NULL);
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (55,'en','Peach','US','United States','America Donut Peach',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (55,'zh','桃','US','美國','美國扁桃',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
 
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (65,'en','Orange','United States','PureSpect Orange',NULL);
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (65,'zh','橙','美國','美國黑牌橙',NULL);
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (65,'en','Orange','US','United States','PureSpect Orange',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (65,'zh','橙','US','美國','美國黑牌橙',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
 
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (75,'en','Grapes','Japan','Shine Muscat',NULL);
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (75,'zh','提子','日本','晴王',NULL);
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (75,'en','Grapes','JP','Japan','Shine Muscat',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (75,'zh','提子','JP','日本','晴王',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
 
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (85,'en','Melon','Japan','Shizuoka Melon',NULL);
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (85,'zh','密瓜','日本','靜岡密瓜',NULL);
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (85,'en','Melon','JP','Japan','Shizuoka Melon',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (85,'zh','密瓜','JP','日本','靜岡密瓜',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
 
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (95,'en','Melon','Japan','The Yubari King',NULL);
-INSERT INTO localized_product(id,locale,category,country,name,unit) VALUES (95,'zh','密瓜','日本','北海道夕張瓜',NULL);
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (95,'en','Melon','JP','Japan','The Yubari King',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
+INSERT INTO localized_product(id,locale,category,country_code,country,name,unit,description) VALUES (95,'zh','密瓜','JP','日本','北海道夕張瓜',NULL,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
  
 SET foreign_key_checks = 1;
 
